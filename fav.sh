04e8804fb5f732588c0cb473ebbfdaf5679ed819
Luka Milic <lukam321@gmail.com>
2016-01-26 23:37:22 +0000

    Update fav.sh

--- fav.sh
+++ fav.sh
@@ -1,6 +1,13 @@
-# Store the location of your favourite directory
-FDPATH=~/Dropbox/.lastWorkDir
+# Example FDPATH
+FDPATH=~/.lastWorkDir
+
+# Putting it in a dropbox is useful if you want to use this on multiple machines
+#FDPATH=~/Dropbox/.lastWorkDir
+
+# Set your favourite directory
 alias fa='echo $(echo $(pwd) | sed "s:^$HOME:~:") > $FDPATH'
+
+
 # Go to your favourite directory
 alias f='cd $(cat $FDPATH | sed "s:^~:$HOME:")'
 
